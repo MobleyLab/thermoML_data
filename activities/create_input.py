f5cff6dcecb47f2a8e6307672ffe3f5665924fd8
gduarter <guilhermedrmatos@gmail.com>
2018-03-02 14:42:19 -0800

    added parsed input files

---
+++ activities/create_input.py
@@ -0,0 +1,8 @@
+# Import general modules
+import pandas as pd
+
+# Import chemistry-related modules
+import openeye.oechem as oechem
+
+tags = ["solvents","molar_fractions","density","temperature","pressure"]
+
