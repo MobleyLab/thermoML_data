fcfe895a43ef656ee7d116a25b65d6b11df43f6a
gduarter <guilhermedrmatos@gmail.com>
2018-03-12 13:42:03 -0700

    removed unrelated project data

--- activities/create_input.py
+++
@@ -1,8 +0,0 @@
-# Import general modules
-import pandas as pd
-
-# Import chemistry-related modules
-import openeye.oechem as oechem
-
-tags = ["solvents","molar_fractions","density","temperature","pressure"]
-
--- oecup_data/README.md
+++
@@ -1,1 +0,0 @@
-Add Orion results here.
